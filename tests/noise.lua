f66739d8299318daa65a2cfb3694fe4257010b96
Elias Hogstvedt <eliashogstvedt@gmail.com>
2019-06-28 22:41:42 +0200

    noise test

---
+++ tests/noise.lua
@@ -0,0 +1,15 @@
+local oh = require("oh.oh")
+
+for i = 1, 100 do
+    math.randomseed(os.clock())
+
+    local code = {}
+
+    for i = 1, 100 do
+        code[i] = string.char(math.random(255))
+    end
+
+    code = table.concat(code)
+
+    print(oh.loadstring(code))
+end
