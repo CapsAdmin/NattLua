ec275714e48afceae608d24bd911b1d772f2d6a2
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-10-07 00:37:16 +0200

    basic coroutine support

---
+++ test/lua/analyzer/coroutines.lua
@@ -0,0 +1,20 @@
+local T = require("test.helpers")
+local run = T.RunCode
+
+run[[
+    local co = coroutine.create(function (a,b,c)
+        type_assert(a, 1)
+        type_assert(b, 2)
+        type_assert(c, 3)
+    end)
+
+    coroutine.resume(co,1,2,3)
+]]
+
+run[[
+    local func = coroutine.wrap(function(a)
+        type_assert(a, 1)
+    end)
+    
+    func(1)
+]]
