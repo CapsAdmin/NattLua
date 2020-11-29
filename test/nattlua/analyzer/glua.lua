1cfded3cc4c5cbb49352b28aabe89b3c7b399f98
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-11-29 13:09:24 +0100

    more glua stuff

--- test/nattlua/analyzer/glua.lua
+++ test/nattlua/analyzer/glua.lua
@@ -2,7 +2,7 @@ local T = require("test.helpers")
 local run = T.RunCode
 
 run[[
-    local { WorldToLocal, Vector, Angle, TVector, TAngle } = loadfile("nattlua/runtime/gmod.nlua")()
+    local { WorldToLocal, Vector, Angle } = loadfile("nattlua/runtime/gmod.nlua")()
     local pos, ang = WorldToLocal(Vector(1,2,3), Angle(1,5,2), Vector(5,6,2), Angle(10235,123,123))
     
     type_assert(pos, _ as TVector)
