b50559b23aedd6015d8794fe1858486fb63e13cd
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-11-30 21:15:12 +0100

    rewrite on_editor_save

--- .vscode/on_editor_save.lua
+++ .vscode/on_editor_save.lua
@@ -1,59 +1,76 @@
 local nl = require("nattlua")
-local path = ...
 
-if path:sub(-4) ~= ".lua" and path:sub(-5) ~= ".nlua" then
-    return
-end
+local did_something = false
 
-local function run(path, ...)
+local function run_lua(path, ...)
+    did_something = true
     assert(loadfile(path))(...)
 end
 
-if path:find("test/") and path:sub(-5) ~= ".nlua" then
-    run("test/run.lua", path)
-    return
+local function run_nattlua(path)
+    did_something = true
+    local c = assert(nl.File(path, {annotate = true}))
+    c:EnableEventDump(true)
+    if c.code:find("%-%-%s-DISABLE_BASE_ENV") then
+        _G.DISABLE_BASE_ENV = true
+    end
+
+    local ok, err = c:Analyze()
+    if c.code:find("--DISABLE_BASE_ENV", nil, true) then
+        _G.DISABLE_BASE_ENV = nil
+    end
+    if not ok then
+        io.write(err, "\n")
+        return
+    end
+    local res = assert(c:Emit())
+    require("nattlua.runtime.base_runtime")
+    io.write("== code result ==\n")
+    io.write(res, "\n")
+    --assert(load(res))()
 end
 
-if path:find("javascript_emitter") then
-    path = "./examples/lua_to_js.lua"
-end
-
-if path:find("/nattlua", nil, true) and not path:find("helpers") then
+local function has_test_focus()
     local f = io.open("test_focus.lua")
     if not f or (f and #f:read("*all") == 0) then
         if f then f:close() end
-        if path:find("/nattlua/", nil, true) then
-            run("test/run.lua", "nattlua")
-        else
-            run("test/run.lua")
-        end
+        return false
+    end
+
+    return true
+end
+
+local path = ...
+
+if path:find("on_editor_save.lua", nil, true) then return end
+
+if path:lower():find("/nattlua/") then
+    if not path then
+        error("no path")
+    end
+
+    local is_lua = path:sub(-4) == ".lua"
+    local is_nattlua = path:sub(-5) == ".nlua"
+
+    if not is_lua and not is_nattlua then
         return
-    else
-        path = "./test_focus.lua"
+    end
+
+    if is_nattlua then
+        run_nattlua(path)
+    elseif has_test_focus() then
+        run_nattlua("./test_focus.lua")
+    elseif path:find("javascript_emitter") then
+        run_lua("./examples/lua_to_js.lua")
+    elseif path:find("/nattlua", nil, true) and not path:find("helpers") then
+        run_lua("test/run.lua")  
+    elseif path:find("examples/") then
+        run_lua(path)
     end
 end
 
-if path:find("examples/") and path:sub(-5) ~= ".nlua" then
-    run(path)
-    return
+if not did_something then
+    print("not sure how to run " .. path)
+    print("running as normal lua")
+    run_lua(path)
 end
-
-local c = assert(nl.File(path, {annotate = true}))
-c:EnableEventDump(true)
-if c.code:find("%-%-%s-DISABLE_BASE_ENV") then
-    _G.DISABLE_BASE_ENV = true
-end
-
-local ok, err = c:Analyze()
-if c.code:find("--DISABLE_BASE_ENV", nil, true) then
-    _G.DISABLE_BASE_ENV = nil
-end
-if not ok then
-    io.write(err, "\n")
-    return
-end
-local res = assert(c:Emit())
-require("nattlua.runtime.base_runtime")
-io.write("== code result ==\n")
-io.write(res, "\n")
---assert(load(res))()
