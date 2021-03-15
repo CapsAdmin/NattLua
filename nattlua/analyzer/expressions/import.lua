6beb310b244e4e0a3f24dc08dec7769b12cb30f2
CapsAdmin <eliashogstvedt@gmail.com>
2021-03-15 09:42:28 +0100

    share fix arguments not being passed in import expression

--- nattlua/analyzer/expressions/import.lua
+++ nattlua/analyzer/expressions/import.lua
@@ -1,10 +1,9 @@
 local types = require("nattlua.types.types")
 
 return function(META)
-    function META:AnalyzeImportExpression(node, env)
-        
+    function META:AnalyzeImportExpression(node, env)        
         local args = self:AnalyzeExpressions(node.expressions, env)
 
-        return self:AnalyzeRootStatement(node.root)
+        return self:AnalyzeRootStatement(node.root, table.unpack(args))
     end
 end
