c8199d0fee97aedd69d61ee328921cc08e18d083
CapsAdmin <eliashogstvedt@gmail.com>
2021-04-01 10:02:01 +0200

    remove unused code

--- test/data_structures/static_array.lua
+++
@@ -1,9 +0,0 @@
-local StaticArrayType = require("nattlua.other.data_structures.static_array")
-local u8 = require("nattlua.other.data_structures.primitives").u8
-
-
-local UInt8Array10 = StaticArrayType(u8, 10)
-local arr = UInt8Array10()
-assert(#arr == 10)
-arr:Set(5, 44)
-assert(arr:Get(5) == 44)
