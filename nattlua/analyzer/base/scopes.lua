9c2d06061c21fc9a32df518a63a4d31a535d9d97
CapsAdmin <eliashogstvedt@gmail.com>
2021-04-18 13:45:09 +0200

    fix global lookup

--- nattlua/analyzer/base/scopes.lua
+++ nattlua/analyzer/base/scopes.lua
@@ -207,7 +207,7 @@ return function(META)
 
 					if self:GetScope():IsReadOnly() then return end
 
-					if not obj and env == "runtime" then
+					if env == "runtime" then
 						self:Warning(key, "_G[\"" .. key:Render() .. "\"] = " .. tostring(val))
 					end
 
