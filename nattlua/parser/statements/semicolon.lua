8cd2d28f71f65133203e73d3d84c4e08cae2046a
CapsAdmin <eliashogstvedt@gmail.com>
2021-05-28 22:56:20 +0200

    refactoring

--- nattlua/parser/statements/semicolon.lua
+++ nattlua/parser/statements/semicolon.lua
@@ -1,5 +1,5 @@
 return function(parser)
-	if not parser:IsCurrentValue(";") then return end
+	if not parser:IsCurrentValue(";") then return nil end
 	local node = parser:Node("statement", "semicolon")
 	node.tokens[";"] = parser:ReadValue(";")
 	return node
--- nattlua/parser/statements/while.lua
+++ nattlua/parser/statements/while.lua
@@ -1,6 +1,6 @@
 return function(parser)
+	if not parser:IsCurrentValue("while") then return nil end
 	return
-		parser:IsCurrentValue("while") and
 		parser:Node("statement", "while"):ExpectKeyword("while"):ExpectExpression():ExpectKeyword("do")
 		:ExpectNodesUntil("end")
 		:ExpectKeyword("end", "do")
