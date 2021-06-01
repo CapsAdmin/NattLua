f9a5ebd5e3569c225da0bd5fc9039a15613c47ba
CapsAdmin <eliashogstvedt@gmail.com>
2021-06-01 22:40:04 +0200

    metatable test

--- test/nattlua/analyzer/metatable.lua
+++ test/nattlua/analyzer/metatable.lua
@@ -557,3 +557,94 @@ run[[
     end
 
 ]]
+
+run[[
+
+    -- class.lua
+    -- Compatible with Lua 5.1 (not 5.0).
+    local function class(base: literal any, init: literal any)
+        local c = {}    -- a new class instance
+        if not init and type(base) == 'function' then
+           init = base
+           base = nil
+        elseif type(base) == 'table' then
+         -- our new class is a shallow copy of the base class!
+           for i,v in pairs(base) do
+              c[i] = v
+           end
+           c._base = base
+        end
+        -- the class will be the metatable for all its objects,
+        -- and they will look up their methods in it.
+        c.__index = c
+     
+        -- expose a constructor which can be called by <classname>(<args>)
+        local mt = {}
+        mt.__call = function(class_tbl, ...)
+            local obj = {}
+            setmetatable(obj,c)
+            if init then
+                init(obj,...)
+            else 
+            -- make sure that any stuff from the base class is initialized!
+            if base and base.init then
+            base.init(obj, ...)
+            end
+            end
+            return obj
+        end
+        c.init = init
+        c.is_a = function(self: literal any, klass: literal any)
+           local m = getmetatable(self)
+           while m do 
+              if m == klass then return true end
+              m = m._base
+           end
+           return false
+        end
+        setmetatable(c, mt)
+        return c
+     end
+    
+     
+    local Animal = class(function(a: literal any,name: literal any)
+        a.name = name
+    end)
+    
+    function Animal:__tostring()
+        return self.name..': '..self:speak()
+    end
+    
+    local Dog = class(Animal)
+    
+    function Dog:speak()
+        return 'bark'
+    end
+    
+    local Cat = class(Animal, function(c: literal any,name: literal any,breed: literal any)
+        Animal.init(c,name)  -- must init base!
+        c.breed = breed
+    end)
+    
+    function Cat:speak()
+        return 'meow'
+    end
+    
+    local Lion = class(Cat)
+    
+    function Lion:speak()
+        return 'roar'
+    end
+        
+    local fido = Dog('Fido')
+    local felix = Cat('Felix','Tabby')
+    local leo = Lion('Leo','African')
+    
+    type_assert(leo:is_a(Animal), true)
+    type_assert(leo:is_a(Cat), true)
+    type_assert(leo:is_a(Dog), false)
+    type_assert(leo:__tostring(), "Leo: roar")
+    type_assert(leo:speak(), "roar")
+
+
+]]
