2804f876f78e5aa21ae457f2e205144f605a4032
CapsAdmin <eliashogstvedt@gmail.com>
2021-07-28 23:58:20 +0200

    test luajit project in the test suite

---
+++ test/nattlua/luajit_project.lua
@@ -0,0 +1,12 @@
+local nl = require("nattlua")
+
+local builder = assert(nl.File("example_projects/luajit/src/test.nlua"))
+
+function builder:OnResolvePath(path)
+    return "example_projects/luajit/src/" .. path
+end
+
+assert(builder:Lex())
+assert(builder:Parse())
+
+assert(builder:Analyze())
