388d8f3e98a74444ed8374f9471ded1ba2546c66
CapsAdmin <eliashogstvedt@gmail.com>
2022-03-14 21:45:57 +0100

    refactoring

--- test/helpers.lua
+++ test/helpers.lua
@@ -1,57 +1,117 @@
 local nl = require("nattlua")
-local types = require("nattlua.types.types")
-local BuildBaseEnvironment = require("nattlua.runtime.base_environment").BuildBaseEnvironment
-types.Initialize()
--- reuse an existing environment to speed up tests
-local runtime_env, typesystem_env = BuildBaseEnvironment()
+local Table = require("nattlua.types.table").Table
+local Union = require("nattlua.types.union").Union
+local Tuple = require("nattlua.types.tuple").Tuple
+local Number = require("nattlua.types.number").Number
+local LNumber = require("nattlua.types.number").LNumber
+local LString = require("nattlua.types.string").LString
+local Function = require("nattlua.types.function").Function
+local String = require("nattlua.types.string").String
+local Any = require("nattlua.types.any").Any
+local Symbol = require("nattlua.types.symbol").Symbol
+local helpers = {}
+helpers.Function = Function
+helpers.Table = Table
+helpers.Any = Any
+helpers.Symbol = Symbol
 
-local function run(code, expect_error, expect_warning)
-	_G.TEST = true
-	local compiler = nl.Compiler(code, nil, nil, 3)
-	compiler:SetEnvironments(runtime_env:Copy(), typesystem_env)
-	local ok, err = compiler:Analyze()
-	_G.TEST = false
+do
+	local function cast(...)
+		local ret = {}
 
-	if expect_warning then
-		local str = ""
+		for i = 1, select("#", ...) do
+			local v = select(i, ...)
+			local t = type(v)
 
-		for _, diagnostic in ipairs(compiler.analyzer.diagnostics) do
-			if diagnostic.msg:find(expect_warning) then return compiler end
-
-			str = str .. diagnostic.msg .. "\n"
+			if t == "number" then
+				ret[i] = LNumber(v)
+			elseif t == "string" then
+				ret[i] = LString(v)
+			elseif t == "boolean" then
+				ret[i] = Symbol(v)
+			else
+				ret[i] = v
+			end
 		end
 
-		if str == "" then error("expected warning, got\n\n\n" .. str, 3) end
-
-		error("expected warning '" .. expect_warning .. "' got:\n>>\n" .. str .. "\n<<", 3)
+		return ret
 	end
 
-	if expect_error then
-		if not err or err == "" then
-			error("expected error, got\n\n\n[" .. tostring(ok) .. ", " .. tostring(err) .. "]", 3)
-		elseif type(expect_error) == "string" then
-			if not err:find(expect_error) then
-				error("expected error '" .. expect_error .. "' got:\n>>\n" .. err .. "\n<<", 3)
-			end
-		elseif type(expect_error) == "function" then
-			local ok, msg = pcall(expect_error, err)
-
-			if not ok then
-				error("error did not pass: " .. msg .. "\n\nthe error message was:\n" .. err, 3)
-			end
-		else
-			error("invalid expect_error argument", 3)
-		end
-	else
-		if not ok then error(err, 3) end
+	function helpers.Union(...)
+		return Union(cast(...))
 	end
 
-	return compiler
+	function helpers.Tuple(...)
+		return Tuple(cast(...))
+	end
 end
 
-local function table_equal(o1, o2, ignore_mt, callList)
+function helpers.Number(n)
+	return Number(n):SetLiteral(n ~= nil)
+end
+
+function helpers.String(n)
+	return String(n):SetLiteral(n ~= nil)
+end
+
+do
+	-- reuse an existing environment to speed up tests
+	local BuildBaseEnvironment = require("nattlua.runtime.base_environment").BuildBaseEnvironment
+	local runtime_env, typesystem_env = BuildBaseEnvironment()
+
+	function helpers.RunCode(code, expect_error, expect_warning)
+		_G.TEST = true
+		local compiler = nl.Compiler(code, nil, nil, 3)
+		compiler:SetEnvironments(runtime_env:Copy(), typesystem_env)
+		local ok, err = compiler:Analyze()
+		_G.TEST = false
+
+		if expect_warning then
+			local str = ""
+
+			for _, diagnostic in ipairs(compiler.analyzer.diagnostics) do
+				if diagnostic.msg:find(expect_warning) then return compiler end
+
+				str = str .. diagnostic.msg .. "\n"
+			end
+
+			if str == "" then error("expected warning, got\n\n\n" .. str, 3) end
+
+			error("expected warning '" .. expect_warning .. "' got:\n>>\n" .. str .. "\n<<", 3)
+		end
+
+		if expect_error then
+			if not err or err == "" then
+				error("expected error, got\n\n\n[" .. tostring(ok) .. ", " .. tostring(err) .. "]", 3)
+			elseif type(expect_error) == "string" then
+				if not err:find(expect_error) then
+					error("expected error '" .. expect_error .. "' got:\n>>\n" .. err .. "\n<<", 3)
+				end
+			elseif type(expect_error) == "function" then
+				local ok, msg = pcall(expect_error, err)
+
+				if not ok then
+					error("error did not pass: " .. msg .. "\n\nthe error message was:\n" .. err, 3)
+				end
+			else
+				error("invalid expect_error argument", 3)
+			end
+		else
+			if not ok then error(err, 3) end
+		end
+
+		return compiler.analyzer, compiler.SyntaxTree
+	end
+end
+
+function helpers.Transpile(code)
+	return helpers.RunCode(code):Emit({annotate = true})
+end
+
+function helpers.TableEqual(o1, o2, ignore_mt, callList)
 	if o1 == o2 then return true end
 
+	callList = callList or {}
 	local o1Type = type(o1)
 	local o2Type = type(o2)
 
@@ -89,7 +149,7 @@ local function table_equal(o1, o2, ignore_mt, callList)
 		local vComparisons = callList[value1]
 
 		if not vComparisons or vComparisons[value2] == nil then
-			if not table_equal(value1, value2, ignore_mt, callList) then
+			if not helpers.TableEqual(value1, value2, ignore_mt, callList) then
 				return false
 			end
 		end
@@ -106,60 +166,4 @@ local function table_equal(o1, o2, ignore_mt, callList)
 	return true
 end
 
-local function cast(...)
-	local ret = {}
-
-	for i = 1, select("#", ...) do
-		local v = select(i, ...)
-		local t = type(v)
-
-		if t == "number" then
-			ret[i] = types.LNumber(v)
-		elseif t == "string" then
-			ret[i] = types.LString(v)
-		elseif t == "boolean" then
-			ret[i] = types.Symbol(v)
-		else
-			ret[i] = v
-		end
-	end
-
-	return ret
-end
-
-return {
-	Union = function(...)
-		return types.Union(cast(...))
-	end,
-	Tuple = function(...)
-		return types.Tuple(cast(...))
-	end,
-	Number = function(n)
-		return types.Number(n):SetLiteral(n ~= nil)
-	end,
-	Function = function(d)
-		return types.Function(d)
-	end,
-	String = function(n)
-		return types.String(n):SetLiteral(n ~= nil)
-	end,
-	Table = function(data)
-		return types.Table(data or {})
-	end,
-	Symbol = function(data)
-		return types.Symbol(data)
-	end,
-	Any = function()
-		return types.Any()
-	end,
-	RunCode = function(code, expect_error, expect_warning)
-		local compiler = run(code, expect_error, expect_warning)
-		return compiler.analyzer, compiler.SyntaxTree
-	end,
-	Transpile = function(code)
-		return run(code):Emit({annotate = true})
-	end,
-	TableEqual = function(o1, o2, ignore_mt)
-		return table_equal(o1, o2, ignore_mt, {})
-	end,
-}
+return helpers
