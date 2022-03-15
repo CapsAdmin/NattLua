a36f6dd4821442895c631be04dde3f9ca8f06300
CapsAdmin <eliashogstvedt@gmail.com>
2022-03-15 05:56:56 +0100

    add github pages publish script

---
+++ language_server/playground/publish.sh
@@ -0,0 +1,11 @@
+rm -rf public
+mkdir public
+cp -r ./* public/
+cd public
+rm -rf node_modules
+git init
+git remote add origin git@github.com:CapsAdmin/NattLua.git
+git checkout -b gh-pages
+git add --all
+git commit -m "Deploy"
+git push -f origin gh-pages
