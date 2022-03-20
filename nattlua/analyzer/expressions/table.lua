f1fac82394541ff1c52ed8acb4124e0c8baf8a6b
CapsAdmin <eliashogstvedt@gmail.com>
2022-03-20 13:56:10 +0100

    fix table spread not being analyzed properly

--- nattlua/analyzer/expressions/table.lua
+++ nattlua/analyzer/expressions/table.lua
@@ -3,6 +3,7 @@ local ipairs = ipairs
 local LNumber = require("nattlua.types.number").LNumber
 local LString = require("nattlua.types.string").LString
 local Table = require("nattlua.types.table").Table
+local Nil = require("nattlua.types.symbol").Nil
 local table = _G.table
 return {
 	AnalyzeTable = function(self, node)
@@ -24,34 +25,44 @@ return {
 				local val = self:AnalyzeExpression(node.value_expression):GetFirstValue()
 				self:NewIndexOperator(node, tbl, key, val)
 			elseif node.kind == "table_index_value" then
-				local obj = self:AnalyzeExpression(node.value_expression)
-
-				if
-					node.value_expression.kind ~= "value" or
-					node.value_expression.value.value ~= "..."
-				then
-					obj = obj:GetFirstValue()
-				end
-
-				if obj.Type == "tuple" then
-					if tree.children[i + 1] then
-						tbl:Insert(obj:Get(1))
-					else
-						for i = 1, obj:GetMinimumLength() do
-							tbl:Set(LNumber(#tbl:GetData() + 1), obj:Get(i))
+				if node.spread then
+					local val = self:AnalyzeExpression(node.spread.expression):GetFirstValue()
+					for _, kv in ipairs(val:GetData()) do
+						local val = kv.val
+						if val.Type == "union" and val:CanBeNil() then
+							val = val:Copy():RemoveType(Nil())
 						end
-
-						if obj.Remainder then
-							local current_index = LNumber(#tbl:GetData() + 1)
-							local max = LNumber(obj.Remainder:GetLength())
-							tbl:Set(current_index:SetMax(max), obj.Remainder:Get(1))
-						end
+						self:NewIndexOperator(node, tbl, kv.key, val)
 					end
 				else
-					if node.i then
-						tbl:Insert(LNumber(obj))
-					elseif obj then
-						tbl:Insert(obj)
+					local obj = self:AnalyzeExpression(node.value_expression)
+					if
+						node.value_expression.kind ~= "value" or
+						node.value_expression.value.value ~= "..."
+					then
+						obj = obj:GetFirstValue()
+					end
+
+					if obj.Type == "tuple" then
+						if tree.children[i + 1] then
+							tbl:Insert(obj:Get(1))
+						else
+							for i = 1, obj:GetMinimumLength() do
+								tbl:Set(LNumber(#tbl:GetData() + 1), obj:Get(i))
+							end
+
+							if obj.Remainder then
+								local current_index = LNumber(#tbl:GetData() + 1)
+								local max = LNumber(obj.Remainder:GetLength())
+								tbl:Set(current_index:SetMax(max), obj.Remainder:Get(1))
+							end
+						end
+					else
+						if node.i then
+							tbl:Insert(LNumber(obj))
+						elseif obj then
+							tbl:Insert(obj)
+						end
 					end
 				end
 			end
