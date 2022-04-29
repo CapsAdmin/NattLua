5771d0b115640c636fe0429882bff7235352ca68
CapsAdmin <eliashogstvedt@gmail.com>
2022-04-29 10:39:49 +0200

    refactoring

--- nattlua/analyzer/operators/call.lua
+++ nattlua/analyzer/operators/call.lua
@@ -30,55 +30,12 @@ return {
 			return truthy_union
 		end
 
-		local function call(self, obj, arguments)
-			if obj.Type == "union" then
-				-- make sure the union is callable, we pass the analyzer and 
-				-- it will throw errors if the union contains something that is not callable
-				-- however it will continue and just remove those values from the union
-				obj = make_callable_union(self, obj)
-			end
 
-			-- if obj is a tuple it will return its first value
-			-- (myfunc,otherfunc)(1) will always
-			obj = obj:GetFirstValue()
-
-			if obj.Type ~= "function" then
-				if obj.Type == "any" then
-					-- it's ok to call any types, it will just return any
-					-- check arguments that can be mutated
-					for _, arg in ipairs(arguments:GetData()) do
-						if arg.Type == "table" and arg:GetAnalyzerEnvironment() == "runtime" then
-							if arg:GetContract() then
-								-- error if we call any with tables that have contracts
-								-- since anything might happen to them in an any call
-								self:Error(
-									{
-										"cannot mutate argument with contract ",
-										arg:GetContract(),
-									}
-								)
-							else
-								-- if we pass a table without a contract to an any call, we add any to its key values
-								for _, keyval in ipairs(arg:GetData()) do
-									keyval.key = Union({Any(), keyval.key})
-									keyval.val = Union({Any(), keyval.val})
-								end
-							end
-						end
-					end
-				end
-
-				return obj:Call(self, arguments, self:GetCallStack()[1].call_node, true)
-			end
-
-			-- mark the object as called so the unreachable code step won't call it
-			obj:SetCalled(true)
-			local function_arguments = obj:GetArguments()
-
+		local function infer_argument_functions(self, obj, input_arguments)
 			-- infer any uncalled functions in the arguments to get their return type
-			for i, b in ipairs(arguments:GetData()) do
+			for i, b in ipairs(input_arguments:GetData()) do
 				if b.Type == "function" and not b:IsCalled() and not b:HasExplicitReturnTypes() then
-					local a = function_arguments:Get(i)
+					local a =  obj:GetArguments():Get(i)
 
 					if
 						a and
@@ -113,24 +70,71 @@ return {
 						end
 					end
 				end
-			end
-
-			if obj:GetData().lua_function then
-				return self:CallAnalyzerFunction(obj, function_arguments, arguments)
-			elseif obj.function_body_node then
-				return self:CallBodyFunction(obj, arguments, obj.function_body_node)
-			end
-
-			return self:CallFunctionSignature(obj, arguments)
+			end	
 		end
 
-		function META:Call(obj, arguments, call_node, not_recursive_call)
+		local function call(self, obj, input_arguments)
+			if obj.Type == "union" then
+				-- make sure the union is callable, we pass the analyzer and 
+				-- it will throw errors if the union contains something that is not callable
+				-- however it will continue and just remove those values from the union
+				obj = make_callable_union(self, obj)
+			end
+
+			-- if obj is a tuple it will return its first value
+			-- (myfunc,otherfunc)(1) will always
+			obj = obj:GetFirstValue()
+
+			if obj.Type == "any" then
+				-- it's ok to call any types, it will just return any
+				-- check arguments that can be mutated
+				for _, arg in ipairs(input_arguments:GetData()) do
+					if arg.Type == "table" and arg:GetAnalyzerEnvironment() == "runtime" then
+						if arg:GetContract() then
+							-- error if we call any with tables that have contracts
+							-- since anything might happen to them in an any call
+							self:Error(
+								{
+									"cannot mutate argument with contract ",
+									arg:GetContract(),
+								}
+							)
+						else
+							-- if we pass a table without a contract to an any call, we add any to its key values
+							for _, keyval in ipairs(arg:GetData()) do
+								keyval.key = Union({Any(), keyval.key})
+								keyval.val = Union({Any(), keyval.val})
+							end
+						end
+					end
+				end
+			end
+			
+			if obj.Type == "function" then
+				-- mark the object as called so the unreachable code step won't call it
+				obj:SetCalled(true)
+
+				infer_argument_functions(self, obj, input_arguments)				
+
+				if obj:GetData().lua_function then
+					return self:CallAnalyzerFunction(obj, obj:GetArguments(), input_arguments)
+				elseif obj.function_body_node then
+					return self:CallBodyFunction(obj, input_arguments, obj.function_body_node)
+				end
+
+				return self:CallFunctionSignature(obj, input_arguments)
+			else
+				return obj:Call(self, input_arguments, self:GetCallStack()[1].call_node, true)
+			end
+		end
+
+		function META:Call(obj, input_arguments, call_node, not_recursive_call)
 			local ok, err = self:PushCallFrame(obj, call_node, not_recursive_call)
 
 			if not ok == false then return ok, err end
 			if ok then return ok end
 
-			local ok, err = call(self, obj, arguments)
+			local ok, err = call(self, obj, input_arguments)
 
 			self:PopCallFrame()
 
