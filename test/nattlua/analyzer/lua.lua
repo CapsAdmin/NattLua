bb2f2b002ef46e9b312c8d020548b8114f4fa766
CapsAdmin <eliashogstvedt@gmail.com>
2022-05-15 22:49:20 +0200

    string tests

--- test/nattlua/analyzer/lua.lua
+++ test/nattlua/analyzer/lua.lua
@@ -453,3 +453,155 @@ analyze[[
     assert(0x1.1 == '0x1.' + '+0x.1')
     assert(0Xabcdef.0 == 0x.ABCDEFp+24)
 ]]
+
+analyze[[
+
+    local undef = nil
+    local type assert = attest.truthy
+    local type pcall = attest.pcall
+    
+    local maxi <const> = math.maxinteger
+    local mini <const> = math.mininteger
+    
+    
+    local function checkerror (msg: ref string, f: ref Function, ...: ref ...any)
+      local s, err = pcall(f, ...)
+      if not (not s and string.find(err, msg)) then
+        error("assertion failed", 2)
+      end
+    end
+    
+    
+    -- testing string comparisons
+    assert('alo' < 'alo1')
+    assert('' < 'a')
+    assert('alo\0alo' < 'alo\0b')
+    assert('alo\0alo\0\0' > 'alo\0alo\0')
+    assert('alo' < 'alo\0')
+    assert('alo\0' > 'alo')
+    assert('\0' < '\1')
+    assert('\0\0' < '\0\1')
+    assert('\1\0a\0a' <= '\1\0a\0a')
+    assert(not ('\1\0a\0b' <= '\1\0a\0a'))
+    assert('\0\0\0' < '\0\0\0\0')
+    assert(not('\0\0\0\0' < '\0\0\0'))
+    assert('\0\0\0' <= '\0\0\0\0')
+    assert(not('\0\0\0\0' <= '\0\0\0'))
+    assert('\0\0\0' <= '\0\0\0')
+    assert('\0\0\0' >= '\0\0\0')
+    assert(not ('\0\0b' < '\0\0a\0'))
+    
+    -- testing string.sub
+    assert(string.sub("123456789",2,4) == "234")
+    assert(string.sub("123456789",7) == "789")
+    assert(string.sub("123456789",7,6) == "")
+    assert(string.sub("123456789",7,7) == "7")
+    assert(string.sub("123456789",0,0) == "")
+    assert(string.sub("123456789",-10,10) == "123456789")
+    assert(string.sub("123456789",1,9) == "123456789")
+    assert(string.sub("123456789",-10,-20) == "")
+    assert(string.sub("123456789",-1) == "9")
+    assert(string.sub("123456789",-4) == "6789")
+    assert(string.sub("123456789",-6, -4) == "456")
+    assert(string.sub("\000123456789",3,5) == "234")
+    assert(("\000123456789"):sub(8) == "789")
+    
+    
+    -- testing string.find
+    assert(string.find("123456789", "345") == 3)
+    local a,b = string.find("123456789", "345")
+    assert(string.sub("123456789", a, b) == "345")
+    assert(string.find("1234567890123456789", "345", 3) == 3)
+    assert(string.find("1234567890123456789", "345", 4) == 13)
+    assert(not string.find("1234567890123456789", "346", 4))
+    assert(string.find("1234567890123456789", ".45", -9) == 13)
+    assert(not string.find("abcdefg", "\0", 5, true))
+    assert(string.find("", "") == 1)
+    assert(string.find("", "", 1) == 1)
+    assert(not string.find('', 'aaa', 1))
+    assert(('alo(.)alo'):find('(.)', 1, true) == 4)
+    
+    assert(string.len("") == 0)
+    assert(string.len("\0\0\0") == 3)
+    assert(string.len("1234567890") == 10)
+    
+    assert(#"" == 0)
+    assert(#"\0\0\0" == 3)
+    assert(#"1234567890" == 10)
+    
+    -- testing string.byte/string.char
+    assert(string.byte("a") == 97)
+    assert(string.byte("\xe4") > 127)
+    assert(string.byte(string.char(255)) == 255)
+    assert(string.byte(string.char(0)) == 0)
+    assert(string.byte("\0") == 0)
+    assert(string.byte("\0\0alo\0x", -1) == string.byte('x'))
+    assert(string.byte("ba", 2) == 97)
+    assert(string.byte("\n\n", 2, -1) == 10)
+    assert(string.byte("\n\n", 2, 2) == 10)
+    assert(string.byte("") == nil)
+    assert(string.byte("hi", -3) == nil)
+    assert(string.byte("hi", 3) == nil)
+    assert(string.byte("hi", 9, 10) == nil)
+    assert(string.byte("hi", 2, 1) == nil)
+    assert(string.char() == "")
+    assert(string.char(0, 255, 0) == "\0\255\0")
+    assert(string.char(0, string.byte("\xe4"), 0) == "\0\xe4\0")
+    assert(string.char(string.byte("\xe4l\0�u", 1, -1)) == "\xe4l\0�u")
+    --assert(string.char(string.byte("\xe4l\0�u", 1, 0)) == "")
+    assert(string.char(string.byte("\xe4l\0�u", -10, 100)) == "\xe4l\0�u")
+    
+    checkerror("out of range", string.char, 256)
+    --checkerror("out of range", string.char, -1)
+    --checkerror("out of range", string.char, math.maxinteger)
+    --checkerror("out of range", string.char, math.mininteger)
+    
+    
+    assert(string.upper("ab\0c") == "AB\0C")
+    assert(string.lower("\0ABCc%$") == "\0abcc%$")
+    assert(string.rep('teste', 0) == '')
+    assert(string.rep('t�s\00t�', 2) == 't�s\0t�t�s\000t�')
+    assert(string.rep('', 10) == '')
+    
+    if string.packsize("i") == 4 then
+      -- result length would be 2^31 (int overflow)
+      checkerror("too large", string.rep, 'aa', (1 << 30))
+      checkerror("too large", string.rep, 'a', (1 << 30), ',')
+    end
+    
+    -- repetitions with separator
+    assert(string.rep('teste', 0, 'xuxu') == '')
+    assert(string.rep('teste', 1, 'xuxu') == 'teste')
+    assert(string.rep('\1\0\1', 2, '\0\0') == '\1\0\1\0\0\1\0\1')
+    assert(string.rep('', 10, '.') == string.rep('.', 9))
+    assert(not pcall(string.rep, "aa", maxi // 2 + 10))
+    assert(not pcall(string.rep, "", maxi // 2 + 10, "aa"))
+    
+    assert(string.reverse"" == "")
+    assert(string.reverse"\0\1\2\3" == "\3\2\1\0")
+    assert(string.reverse"\0001234" == "4321\0")
+    
+    for i=0,30 do assert(string.len(string.rep('a', i)) == i) end
+    
+    assert(type(tostring(nil)) == 'string')
+    assert(type(tostring(12)) == 'string')
+    assert(not not string.find(tostring{}, 'table:'))
+    assert(not not string.find(tostring(print), 'function:'))
+    assert(#tostring('\0') == 1)
+    assert(tostring(true) == "true")
+    assert(tostring(false) == "false")
+    assert(tostring(-1203) == "-1203")
+    assert(tostring(1203.125) == "1203.125")
+    assert(tostring(-0.5) == "-0.5")
+    assert(tostring(-32767) == "-32767")
+    
+    if tostring(0.0) == "0.0" then   -- "standard" coercion float->string
+      assert('' .. 12 == '12' and 12.0 .. '' == '12.0')
+      assert(tostring(-1203 + 0.0) == "-1203.0")
+    else   -- compatible coercion
+      assert(tostring(0.0) == "0")
+      assert('' .. 12 == '12' and 12.0 .. '' == '12')
+      assert(tostring(-1203 + 0.0) == "-1203")
+    end
+
+]]
