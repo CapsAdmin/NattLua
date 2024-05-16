c850b8dbb09ace077d7deca98e319892ae0ad966
Elias Hogstvedt <eliashogstvedt@gmail.com>
2024-05-16 11:07:08 +0700

    more work

--- nattlua/c_declarations/analyzer.lua
+++ nattlua/c_declarations/analyzer.lua
@@ -24,7 +24,7 @@ end
 
 function META:WalkTypedef(node)
 	for _, decl in ipairs(node.decls) do
-		self:WalkCDeclaration(decl)
+		self:WalkCDeclaration(decl, true)
 	end
 end
 
@@ -153,7 +153,7 @@ function META:WalkCDeclaration_(node, walk_up)
 	end
 end
 
-function META:WalkCDeclaration(node)
+function META:WalkCDeclaration(node, typedef)
 	local real_node = node
 
 	while node.expression do -- find the inner most expression
@@ -162,11 +162,11 @@ function META:WalkCDeclaration(node)
 
 	self.cdecl = {type = "root", of = nil}
 	local cdecl = self.cdecl
-	self:WalkCDeclaration_(node, true, out)
-	self.Callback(cdecl.of, real_node)
+	self:WalkCDeclaration_(node, true)
+	self.Callback(cdecl.of, real_node, typedef)
 end
 
-local function cast(self, node)
+local function cast(self, node, out)
 	local env = self.env
 	local analyzer = self.analyzer
 	local typs = self.typs
@@ -175,7 +175,7 @@ local function cast(self, node)
 		return (
 			env.FFIArray:Call(
 				analyzer,
-				Tuple({LNumber(tonumber(node.size) or math.huge), cast(self, assert(node.of))})
+				Tuple({LNumber(tonumber(node.size) or math.huge), cast(self, assert(node.of), out)})
 			):Unpack()
 		)
 	elseif node.type == "pointer" then
@@ -183,7 +183,7 @@ local function cast(self, node)
 			return Any() -- TODO: is this true?
 		end
 
-		local res = (env.FFIPointer:Call(analyzer, Tuple({cast(self, assert(node.of))})):Unpack())
+		local res = (env.FFIPointer:Call(analyzer, Tuple({cast(self, assert(node.of), out)})):Unpack())
 		
 		if node.of.type == "type" and node.of.modifiers[1] == "const" and node.of.modifiers[2] == "char" then
 			if self.FUNCTION_ARGUMENT then 
@@ -195,6 +195,49 @@ local function cast(self, node)
 	elseif node.type == "type" then
 		for _, v in ipairs(node.modifiers) do
 			if type(v) == "table" then
+
+				-- only catch struct, union and enum TYPE declarations
+				if v.type == "struct" or v.type == "union" then
+					local tbl
+
+					if v.fields then
+						tbl = Table()
+
+
+						local ident = v.identifier
+
+						if not ident and #node.modifiers > 0 then
+							ident = node.modifiers[#node.modifiers]
+						end
+
+						self.current_nodes = self.current_nodes or {}
+						table.insert(self.current_nodes, 1, {ident = ident, tbl = tbl})
+						
+						--tbl:Set(LString("__id"), LString(("%p"):format({})))
+						for _, v in ipairs(v.fields) do
+							tbl:Set(LString(v.identifier), cast(self, v, out))
+						end
+
+						table.remove(self.current_nodes, 1)
+
+						table.insert(out, {identifier = ident, obj = tbl})
+					else
+						tbl = typs:Get(LString(v.identifier)) or Table()
+						table.insert(out, {identifier = v.identifier, obj = tbl})
+					end
+				elseif v.type == "enum" then
+					local tbl = Table()
+					local i = 0
+
+					for _, v in ipairs(v.fields) do
+						tbl:Set(LString(v.identifier), LNumber(i))
+						i = i + 1
+					end
+
+					table.insert(out, {identifier = v.identifier, obj = tbl})
+				end
+
+				-- catch variable declarations
 				if v.type == "struct" or v.type == "union" then
 					local ident = v.identifier
 
@@ -208,13 +251,13 @@ local function cast(self, node)
 						tbl = Table()
 
 						for _, v in ipairs(v.fields) do
-							tbl:Set(LString(v.identifier), cast(self, v))
+							tbl:Set(LString(v.identifier), cast(self, v, out))
 						end
 					end
 
 					if not tbl then
 						local current = self.current_nodes[1]
-						if current.ident == ident then
+						if current and current.ident == ident then
 							-- recursion
 							tbl = current.tbl
 						end
@@ -299,164 +342,40 @@ local function cast(self, node)
 
 		self.FUNCTION_ARGUMENT = true
 		for i, v in ipairs(node.args) do
-			table.insert(args, cast(self, v))
+			table.insert(args, cast(self, v, out))
 		end
 		self.FUNCTION_ARGUMENT = false
 
-		return (Function(Tuple(args), Tuple({cast(self, assert(node.rets))})))
+		return (Function(Tuple(args), Tuple({cast(self, assert(node.rets), out)})))
 	elseif node.type == "root" then
-		return (cast(self, assert(node.of)))
+		return (cast(self, assert(node.of), out))
 	else
 		error("unknown type " .. node.type)
 	end
 end
 
-local function cast_type(self, node, out)
-	local typs = self.typs
-
-	if node.type == "array" then
-		cast_type(self, node.of, out)
-	elseif node.type == "pointer" then
-		cast_type(self, node.of, out)
-	elseif node.type == "type" then
-		for _, v in ipairs(node.modifiers) do
-			if type(v) == "table" then
-				if v.type == "struct" or v.type == "union" then
-					local tbl
-
-					if v.fields then
-						tbl = Table()
-
-
-						local ident = v.identifier
-
-						if not ident and #node.modifiers > 0 then
-							ident = node.modifiers[#node.modifiers]
-						end
-
-						self.current_nodes = self.current_nodes or {}
-						table.insert(self.current_nodes, 1, {ident = ident, tbl = tbl})
-						
-						--tbl:Set(LString("__id"), LString(("%p"):format({})))
-						for _, v in ipairs(v.fields) do
-							tbl:Set(LString(v.identifier), cast(self, v))
-						end
-
-						table.remove(self.current_nodes, 1)
-
-						table.insert(out, {identifier = ident, obj = tbl})
-					else
-						tbl = typs:Get(LString(v.identifier)) or Table()
-						table.insert(out, {identifier = v.identifier, obj = tbl})
-					end
-				elseif v.type == "enum" then
-					local tbl = Table()
-					local i = 0
-
-					for _, v in ipairs(v.fields) do
-						tbl:Set(LString(v.identifier), LNumber(i))
-						i = i + 1
-					end
-
-					table.insert(out, {identifier = v.identifier, obj = tbl})
-				else
-					error("unknown type " .. v.type)
-				end
-			end
-		end
-
-		local t = node.modifiers[1]
-
-		if
-			t == "double" or
-			t == "float" or
-			t == "int8_t" or
-			t == "uint8_t" or
-			t == "int16_t" or
-			t == "uint16_t" or
-			t == "int32_t" or
-			t == "uint32_t" or
-			t == "char" or
-			t == "signed char" or
-			t == "unsigned char" or
-			t == "short" or
-			t == "short int" or
-			t == "signed short" or
-			t == "signed short int" or
-			t == "unsigned short" or
-			t == "unsigned short int" or
-			t == "int" or
-			t == "signed" or
-			t == "signed int" or
-			t == "unsigned" or
-			t == "unsigned int" or
-			t == "long" or
-			t == "long int" or
-			t == "signed long" or
-			t == "signed long int" or
-			t == "unsigned long" or
-			t == "unsigned long int" or
-			t == "float" or
-			t == "double" or
-			t == "long double" or
-			t == "size_t" or
-			t == "intptr_t" or
-			t == "uintptr_t"
-		then
-			return Number()
-		elseif
-			t == "int64_t" or
-			t == "uint64_t" or
-			t == "long long" or
-			t == "long long int" or
-			t == "signed long long" or
-			t == "signed long long int" or
-			t == "unsigned long long" or
-			t == "unsigned long long int"
-		then
-			return Number()
-		elseif t == "bool" or t == "_Bool" then
-			return Boolean()
-		elseif t == "void" then
-			return Nil()
-		elseif t == "$" then
-			local res = table.remove(self.dollar_signs_typs, 1)
-			return res
-		elseif t == "va_list" then
-			return Tuple({}):AddRemainder(Tuple({Any()}):SetRepeat(math.huge))
-		end
-
-		return (Number())
-	elseif node.type == "function" then
-		for i, v in ipairs(node.args) do
-			cast_type(self, v, out)
-		end
-
-		cast_type(self, node.rets, out)
-	elseif node.type == "root" then
-		return cast_type(self, node.of, out)
-	else
-		error("unknown type " .. node.type)
-	end
-end
 
 function META:AnalyzeRoot(ast, vars, typs)
 	vars = vars or Table()
 	typs = typs or Table()
 	self.typs = typs
-	self.Callback = function(node, real_node)
+	self.Callback = function(node, real_node, typedef)		
 		local out = {}
-		cast_type(self, node, out)
-
+		local obj = cast(self, node, out)
+		if typedef then
+			typs:Set(LString(real_node.tokens["potential_identifier"].value), obj)
+		else
+			vars:Set(LString(real_node.tokens["potential_identifier"] and real_node.tokens["potential_identifier"].value or "uhoh"), obj)
+		end
+		
 		for _, typedef in ipairs(out) do
 			typs:Set(LString(assert(typedef.identifier)), typedef.obj)
 		end
 		
-		local obj = cast(self, node)
-		vars:Set(LString(real_node.tokens["potential_identifier"] and real_node.tokens["potential_identifier"].value or "uhoh"), obj)
-
 	end
 	self:WalkRoot(ast)
+
+	print(vars, typs)
 	return vars, typs
 end
 
