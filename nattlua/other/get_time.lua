1a28321fd6d74f8fedbdb085ede3c49195839d2e
CapsAdmin <eliashogstvedt@gmail.com>
2024-09-23 22:54:05 +0700

    use a more accurate timer for timing tests

---
+++ nattlua/other/get_time.lua
@@ -0,0 +1,61 @@
+
+local has_ffi, ffi = pcall(require, "ffi")
+
+if not has_ffi then
+    return os.clock
+end
+
+if ffi.os == "OSX" then
+	ffi.cdef([[
+		struct mach_timebase_info {
+			uint32_t	numer;
+			uint32_t	denom;
+		};
+		int mach_timebase_info(struct mach_timebase_info *info);
+		uint64_t mach_absolute_time(void);
+	]])
+	local tb = ffi.new("struct mach_timebase_info")
+	ffi.C.mach_timebase_info(tb)
+	local orwl_timebase = tb.numer
+	local orwl_timebase = tb.denom
+	local orwl_timestart = ffi.C.mach_absolute_time()
+
+	return function()
+		local diff = (ffi.C.mach_absolute_time() - orwl_timestart) * orwl_timebase
+		diff = tonumber(diff) / 1000000000
+		return diff
+	end
+elseif ffi.os == "Windows" then
+    ffi.cdef([[
+		int QueryPerformanceFrequency(int64_t *lpFrequency);
+		int QueryPerformanceCounter(int64_t *lpPerformanceCount);
+	]])
+	local q = ffi.new("int64_t[1]")
+	ffi.C.QueryPerformanceFrequency(q)
+	local freq = tonumber(q[0])
+	local start_time = ffi.new("int64_t[1]")
+	ffi.C.QueryPerformanceCounter(start_time)
+
+	function system.GetTime()
+		local time = ffi.new("int64_t[1]")
+		ffi.C.QueryPerformanceCounter(time)
+		time[0] = time[0] - start_time[0]
+		return tonumber(time[0]) / freq
+	end
+else
+	ffi.cdef([[
+		struct timespec {
+			long int tv_sec;
+			long tv_nsec;
+		};
+		int clock_gettime(int clock_id, struct timespec *tp);
+	]])
+	local ts = ffi.new("struct timespec")
+	local enum = 1
+	local func = ffi.C.clock_gettime
+
+	return function()
+		func(enum, ts)
+		return tonumber(ts.tv_sec) + tonumber(ts.tv_nsec) * 0.000000001
+	end
+end
--- test/run.lua
+++ test/run.lua
@@ -1,6 +1,7 @@
 local preprocess = require("nattlua.other.preprocess")
 local coverage = require("nattlua.other.coverage")
 local profiler = require("nattlua.other.profiler")
+local get_time = require("nattlua.other.get_time")
 local io = require("io")
 local io_write = _G.ON_EDITOR_SAVE and function(...) end or io.write
 local pcall = _G.pcall
@@ -80,27 +81,31 @@ end
 
 if path and path:sub(-4) == ".lua" then
 	io_write(path, " ")
-	local time = os.clock()
+	local time = get_time()
 	assert(loadfile(path))()
-	io_write(" ", format_time(os.clock() - time), " seconds\n")
+	io_write(" ", format_time(get_time() - time), " seconds\n")
 else
 	local tests = find_tests(path)
 
 	for _, path in ipairs(tests) do
 		if path:sub(-4) == ".lua" then
+			local func = assert(loadfile(path))
 			io_write(path, " ")
-			local time = os.clock()
-			assert(loadfile(path))()
-			io_write(" ", format_time(os.clock() - time), " seconds\n")
+			local time = get_time()
+			func()
+			io_write(" ", format_time(get_time() - time), " seconds\n")
 		end
 	end
 
 	for _, path in ipairs(tests) do
 		if path:sub(-5) == ".nlua" then
-			local time = os.clock()
 			io_write(path, " ")
-			analyze(io.open(path, "r"):read("*all"))
-			io_write(" ", format_time(os.clock() - time), " seconds\n")
+			local f = assert(io.open(path, "r"))
+			local str = f:read("*all")
+			local time = get_time()
+			analyze(str)
+			f:close()
+			io_write(" ", format_time(get_time() - time), " seconds\n")
 		end
 	end
 
