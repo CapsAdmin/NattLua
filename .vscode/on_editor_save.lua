6937d7c4d461a08e89a8b1d040e7fe4edfdd5632
CapsAdmin <eliashogstvedt@gmail.com>
2024-10-03 08:33:15 +0700

    refactoring

--- .vscode/on_editor_save.lua
+++ .vscode/on_editor_save.lua
@@ -1,24 +1,29 @@
-_G.ON_EDITOR_SAVE = true
 local nl = require("nattlua")
-local did_something = false
+_G.ON_EDITOR_SAVE = true
+local path = ...
+
+if not path then error("no path") end
+
+local full_path = path
+path = path:lower():gsub("\\", "/")
 
 local function run_lua(path, ...)
-	did_something = true
 	io.write("running ", path, ...)
 	assert(loadfile(path))(...)
 	io.write(" - ok\n")
 end
 
 local function run_nattlua(path)
-	did_something = true
-
-	if assert(io.open(path, "r")):read("*all"):find("%-%-%s-PLAIN_LUA") then
-		return assert(loadfile(path))()
-	end
-
 	local f = assert(io.open(path, "r"))
 	local lua_code = f:read("*all")
 	f:close()
+
+	local function has_flag(flag)
+		return lua_code:find("--" .. flag .. "\n", nil, true) ~= nil
+	end
+
+	if has_flag("PLAIN_LUA") then return assert(loadfile(path))() end
+
 	local c = assert(
 		nl.File(
 			path,
@@ -29,44 +34,35 @@ local function run_nattlua(path)
 			}
 		)
 	)
-	local preserve_whitespace = nil
+	c.debug = has_flag("VERBOSE_STACKTRACE")
+	_G.DISABLE_BASE_ENV = has_flag("DISABLE_BASE_ENV")
 
-	if lua_code:find("%-%-%s-PRETTY_PRINT") then preserve_whitespace = false end
-
-	if lua_code:find("%-%-%s-VERBOSE_STACKTRACE") then c.debug = true end
-
-	if lua_code:find("%-%-%s-DISABLE_BASE_ENV") then
-		_G.DISABLE_BASE_ENV = true
-	end
-
-	if lua_code:find("%-%-%s-PROFILE") then require("jit.p").start("Flp") end
+	if has_flag("PROFILE") then require("jit.p").start("Flp") end
 
 	local ok, err
 
-	if not lua_code:find("%-%-%s-DISABLE_ANALYSIS") then ok, err = c:Analyze() end
+	if not has_flag("DISABLE_ANALYSIS") then ok, err = c:Analyze() end
 
-	if lua_code:find("--DISABLE_BASE_ENV", nil, true) then
-		_G.DISABLE_BASE_ENV = nil
-	end
+	if _G.DISABLE_BASE_ENV then _G.DISABLE_BASE_ENV = nil end
 
-	if lua_code:find("%-%-%s-PROFILE") then require("jit.p").stop() end
+	if has_flag("PROFILE") then require("jit.p").stop() end
 
 	if not ok and err then
-		io.write(err, "\n")
+		error(err)
 		return
 	end
 
 	local res = assert(
 		c:Emit(
 			{
-				preserve_whitespace = preserve_whitespace,
+				preserve_whitespace = has_flag("PRETTY_PRINT") and false or nil,
 				string_quote = "\"",
 				no_semicolon = true,
-				transpile_extensions = lua_code:find("%-%-%s-TRANSPILE_EXTENSIONS") ~= nil,
-				comment_type_annotations = lua_code:find("%-%-%s-COMMENT_TYPE_ANNOTATIONS") ~= nil,
+				transpile_extensions = has_flag("TRANSPILE_EXTENSIONS"),
+				comment_type_annotations = has_flag("COMMENT_TYPE_ANNOTATIONS"),
 				type_annotations = true,
 				force_parenthesis = true,
-				omit_invalid_code = lua_code:find("%-%-%s-OMIT_INVALID_LUA_CODE") ~= nil,
+				omit_invalid_code = has_flag("OMIT_INVALID_LUA_CODE"),
 				extra_indent = {
 					Start = {to = "Stop"},
 					Toggle = "toggle",
@@ -75,125 +71,115 @@ local function run_nattlua(path)
 		)
 	)
 
-	if lua_code:find("%-%-%s-ENABLE_CODE_RESULT_TO_FILE") then
+	if has_flag("ENABLE_CODE_RESULT_TO_FILE") then
 		local f = assert(io.open("test_focus_result.lua", "w"))
 		f:write(res)
 		f:close()
-	elseif lua_code:find("%-%-%s-ENABLE_CODE_RESULT") then
-		io.write("== code result ==\n")
+	elseif has_flag("ENABLE_CODE_RESULT") then
+		print("== code result ==")
 
-		if lua_code:find("%-%-%s-SHOW_NEWLINES") then
-			res = res:gsub("\n", "⏎\n")
-		end
+		if has_flag("SHOW_NEWLINES") then res = res:gsub("\n", "⏎\n") end
 
-		io.write(res, "\n")
-		io.write("=================\n")
+		print(res)
+		print("=================")
 	end
 
-	if lua_code:find("%-%-%s-RUN_CODE") then assert(load(res))() end
+	if has_flag("RUN_CODE") then assert(load(res))() end
 end
 
 local function has_test_focus()
 	local f = io.open("test_focus.nlua")
 
-	if not f or (f and #f:read("*all") == 0) then
-		if f then f:close() end
+	if not f then return false end
 
-		return false
-	end
+	local str = f:read("*all")
+	str = str:gsub("%s+", "")
+	f:close()
+
+	if str == "" then return false end
 
 	return true
 end
 
-local path = ...
-local normalized = path:lower():gsub("\\", "/")
-
-if normalized:find("on_editor_save.lua", nil, true) then return end
-
-if normalized:find("/nattlua/", nil, true) then
-	if not path then error("no path") end
-
-	local is_lua = path:sub(-4) == ".lua"
-	local is_nattlua = path:sub(-5) == ".nlua"
-
-	if not is_lua and not is_nattlua then return end
-
-	if normalized:find("jit_options", nil, true) then
-		run_lua("test/performance/tests.lua")
-	elseif normalized:find("test/.+/.+%.nlua") then
-		run_nattlua(path)
-	elseif
-		normalized:find("jit_trace_track", nil, true) or
-		normalized:find("test/performance/analyzer.lua", nil, true)
-	then
-		run_lua("test/performance/analyzer.lua")
-	elseif normalized:find("c_declarations/main.lua", nil, true) and not has_test_focus() then
-		run_lua("test/run.lua", "test/tests/nattlua/c_declarations/cdef.nlua")
-		run_lua("test/run.lua", "test/tests/nattlua/c_declarations/parsing.lua")
-		run_lua("test/run.lua", "test/tests/nattlua/analyzer/typed_ffi.lua")
-		run_lua("examples/projects/luajit/build.lua", path)
-		run_lua("examples/projects/love2d/nlconfig.lua", path)
-	elseif normalized:find("c_declarations/analyzer", nil, true) and not has_test_focus() then
-		run_lua("test/run.lua", "test/tests/nattlua/c_declarations/cdef.nlua")
-	elseif normalized:find("c_declarations", nil, true) and not has_test_focus() then
-		run_lua("test/run.lua", "test/tests/nattlua/c_declarations/parsing.lua")
-	elseif normalized:find("other/coverage", nil, true) then
-		run_lua("test/run.lua", "test/tests/coverage.lua")
-	elseif normalized:find("nattlua/editor_helper/editor.lua", nil, true) then
-		run_lua("test/run.lua", "test/tests/lsp/editor.lua")
-		os.execute("luajit nattlua.lua build fast && luajit nattlua.lua install")
-		return
-	elseif normalized:find("language_server/server", nil, true) then
-		os.execute("luajit nattlua.lua build fast && luajit nattlua.lua install")
-		return
-	elseif normalized:find("typed_ffi.nlua", nil, true) and has_test_focus() then
-		print("running test focus")
-		run_nattlua("./test_focus.nlua")
-	elseif normalized:find("lint.lua", nil, true) then
-		run_lua(path)
-	elseif normalized:find("build_glua_base.lua", nil, true) then
-		run_lua(path)
-	elseif
-		normalized:find("examples/projects/luajit/", nil, true) or
-		normalized:find("cparser.lua", nil, true)
-	then
-		run_lua("examples/projects/luajit/build.lua", path)
-	elseif normalized:find("examples/projects/love2d/", nil, true) then
-		run_lua("examples/projects/love2d/nlconfig.lua", path)
-	elseif is_nattlua and not normalized:find("/definitions/", nil, true) then
-		run_nattlua(path)
-	elseif normalized:find("formating.lua", nil, true) then
-		run_lua("test/run.lua", "test/tests/helper_test.lua")
-	elseif normalized:find("test/", nil, true) then
-		run_lua("test/run.lua", path)
-	elseif normalized:find("javascript_emitter") then
-		run_lua("./examples/lua_to_js.lua")
-	elseif normalized:find("examples/", nil, true) then
-		run_lua(path)
-	elseif has_test_focus() then
-		print("running test focus")
-		run_nattlua("./test_focus.nlua")
-	elseif
-		(
-			normalized:find("/nattlua/nattlua/", nil, true) or
-			normalized:find("/nattlua/nattlua.lua", nil, true)
-		) and
-		not normalized:find("nattlua/other")
-	then
-		if normalized:find("lexer.lua", nil, true) then
-			run_lua("test/run.lua", "test/tests/nattlua/lexer.lua")
-			run_lua("test/run.lua", "test/performance/lexer.lua")
-		elseif normalized:find("parser.lua", nil, true) and false then
-			run_lua("test/run.lua", "test/tests/nattlua/parser.lua")
-			run_lua("test/run.lua", "test/performance/parser.lua")
-		else
-			run_lua("test/run.lua")
-		end
-	end
+local function run_test(path)
+	return run_lua("test/run.lua", path)
 end
 
-if not did_something then
-	print("not sure how to run " .. path)
+local function find(str)
+	return path:find(str, nil, true) ~= nil
+end
+
+if find("on_editor_save.lua") then return end
+
+local is_lua = full_path:sub(-4) == ".lua"
+local is_nattlua = full_path:sub(-5) == ".nlua"
+local test_focus = has_test_focus()
+
+if not is_lua and not is_nattlua then return end
+
+if full_path:lower():find("/nattlua/", nil, true) == nil and is_lua then
+	print("not sure how to run (/nattlua/ not found in path) " .. full_path)
 	print("running as normal lua")
-	run_lua(path)
+	run_lua(full_path)
+	return
+end
+
+if is_nattlua then
+	run_nattlua(full_path)
+	return
+end
+
+if find("jit_options") then
+	run_lua("test/performance/tests.lua")
+elseif find("jit_trace_track") or find("test/performance/analyzer.lua") then
+	run_lua("test/performance/analyzer.lua")
+elseif find("c_declarations/main.lua") and not test_focus then
+	run_test("test/tests/nattlua/c_declarations/cdef.nlua")
+	run_test("test/tests/nattlua/c_declarations/parsing.lua")
+	run_test("test/tests/nattlua/analyzer/typed_ffi.lua")
+	run_lua("examples/projects/luajit/build.lua", full_path)
+	run_lua("examples/projects/love2d/nlconfig.lua", full_path)
+elseif find("c_declarations/analyzer") and not test_focus then
+	run_test("test/tests/nattlua/c_declarations/cdef.nlua")
+elseif find("c_declarations") and not test_focus then
+	run_test("test/tests/nattlua/c_declarations/parsing.lua")
+elseif find("other/coverage") then
+	run_test("test/tests/coverage.lua")
+elseif find("nattlua/editor_helper/editor.lua") then
+	run_test("test/tests/lsp/editor.lua")
+	os.execute("luajit nattlua.lua build fast && luajit nattlua.lua install")
+elseif find("language_server/server") then
+	os.execute("luajit nattlua.lua build fast && luajit nattlua.lua install")
+elseif find("typed_ffi.nlua") and test_focus then
+	print("running test focus")
+	run_nattlua("./test_focus.nlua")
+elseif find("lint.lua") then
+	run_lua(full_path)
+elseif find("build_glua_base.lua") then
+	run_lua(full_path)
+elseif find("examples/projects/luajit/") or find("cparser.lua") then
+	run_lua("examples/projects/luajit/build.lua", full_path)
+elseif find("examples/projects/love2d/") then
+	run_lua("examples/projects/love2d/nlconfig.lua", full_path)
+elseif is_nattlua and not find("/definitions/") then
+	run_nattlua(full_path)
+elseif find("formating.lua") then
+	run_test("test/tests/helper_test.lua")
+elseif find("test/") then
+	run_test(full_path)
+elseif find("javascript_emitter") then
+	run_lua("./examples/lua_to_js.lua")
+elseif find("examples/") then
+	run_lua(full_path)
+elseif test_focus then
+	print("running test focus")
+	run_nattlua("./test_focus.nlua")
+elseif find("lexer.lua") then
+	run_test("test/tests/nattlua/lexer.lua")
+	run_test("test/performance/lexer.lua")
+elseif find("parser.lua") then
+	run_test("test/tests/nattlua/parser.lua")
+	run_test("test/performance/parser.lua")
+else
+	run_lua("test/run.lua")
 end
