dc79fcda41e39f513d48bf54d6dc32ed7b718e9e
CapsAdmin <eliashogstvedt@gmail.com>
2025-04-18 07:22:27 +0700

    rearrange if statements based on frequency

--- nattlua/analyzer/analyzer.lua
+++ nattlua/analyzer/analyzer.lua
@@ -30,7 +30,28 @@ do
 
 		if node.kind == "assignment" or node.kind == "local_assignment" then
 			AnalyzeAssignment(self, node)
-		elseif
+		elseif node.kind == "if" then
+			AnalyzeIf(self, node)
+		elseif node.kind == "while" then
+			AnalyzeWhile(self, node)
+		elseif node.kind == "do" then
+			self:CreateAndPushScope()
+			self:AnalyzeStatements(node.statements)
+			self:PopScope()
+		elseif node.kind == "repeat" then
+			self:CreateAndPushScope()
+			self:AnalyzeStatements(node.statements)
+			self:PopScope()
+		elseif node.kind == "return" then
+			local ret = self:AnalyzeExpressions(node.expressions)
+			self:Return(node, ret)
+		elseif node.kind == "break" then
+			self:Break()
+		elseif node.kind == "call_expression" then
+			self:AnalyzeExpression(node.value)
+		elseif node.kind == "continue" then
+			self._continue_ = true
+		elseif 
 			node.kind == "destructure_assignment" or
 			node.kind == "local_destructure_assignment"
 		then
@@ -64,27 +85,6 @@ do
 			end
 
 			self:PopAnalyzerEnvironment()
-		elseif node.kind == "if" then
-			AnalyzeIf(self, node)
-		elseif node.kind == "while" then
-			AnalyzeWhile(self, node)
-		elseif node.kind == "do" then
-			self:CreateAndPushScope()
-			self:AnalyzeStatements(node.statements)
-			self:PopScope()
-		elseif node.kind == "repeat" then
-			self:CreateAndPushScope()
-			self:AnalyzeStatements(node.statements)
-			self:PopScope()
-		elseif node.kind == "return" then
-			local ret = self:AnalyzeExpressions(node.expressions)
-			self:Return(node, ret)
-		elseif node.kind == "break" then
-			self:Break()
-		elseif node.kind == "continue" then
-			self._continue_ = true
-		elseif node.kind == "call_expression" then
-			self:AnalyzeExpression(node.value)
 		elseif node.kind == "generic_for" then
 			AnalyzeGenericFor(self, node)
 		elseif node.kind == "numeric_for" then
@@ -126,21 +126,8 @@ do
 	function META:AnalyzeRuntimeExpression(node)
 		self.current_expression = node
 
-		if not node then debug.trace() end
-
 		if node.kind == "value" then
 			return AnalyzeAtomicValue(self, node)
-		elseif node.kind == "vararg" then
-			return VarArg(self:AnalyzeExpression(node.value))
-		elseif
-			node.kind == "function" or
-			node.kind == "analyzer_function" or
-			node.kind == "type_function" or
-			node.kind == "function_signature"
-		then
-			return AnalyzeFunction(self, node)
-		elseif node.kind == "table" or node.kind == "type_table" then
-			return AnalyzeTable(self, node)
 		elseif node.kind == "binary_operator" then
 			return self:AssertWithNode(node, Binary(self, node))
 		elseif node.kind == "prefix_operator" then
@@ -154,11 +141,8 @@ do
 
 			self.current_expression = node
 			return self:Assert(Prefix(self, node, r))
-		elseif node.kind == "postfix_operator" then
-			if node.value.value == "++" then
-				local r = self:AnalyzeExpression(node.left)
-				return Binary(self, setmetatable({value = {value = "+"}}, Node), r, r)
-			end
+		elseif node.kind == "postfix_call" then
+			return AnalyzePostfixCall(self, node)
 		elseif node.kind == "postfix_expression_index" then
 			if self:IsTypesystem() then
 				return self:Assert(
@@ -172,8 +156,25 @@ do
 					)
 				)
 			end
-		elseif node.kind == "postfix_call" then
-			return AnalyzePostfixCall(self, node)
+		elseif 
+			node.kind == "table" or 
+			node.kind == "type_table" 
+		then
+			return AnalyzeTable(self, node)
+		elseif
+			node.kind == "function" or
+			node.kind == "analyzer_function" or
+			node.kind == "type_function" or
+			node.kind == "function_signature"
+		then
+			return AnalyzeFunction(self, node)
+		elseif node.kind == "vararg" then
+			return VarArg(self:AnalyzeExpression(node.value))
+		elseif node.kind == "postfix_operator" then
+			if node.value.value == "++" then
+				local r = self:AnalyzeExpression(node.left)
+				return Binary(self, setmetatable({value = {value = "+"}}, Node), r, r)
+			end
 		elseif node.kind == "empty_union" then
 			return Union()
 		elseif node.kind == "tuple" then
